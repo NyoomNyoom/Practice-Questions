86b67280f3afec16cdb5f2314e64c77ed53c6bcb
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-04-08 23:14:14 +1200

    Adding comments

--- ProjectEuler/ProjectEuler/Q3.c
+++ ProjectEuler/ProjectEuler/Q3.c
@@ -4,13 +4,15 @@
 
 //Find the largest prime factor of a number.
 
+// Works by trying to divide the final number by a factor with no remainder,
+// when it reaches a factor that is larger than half of the current number, 
+// the code prints that value as it is the largest prime factor of x number.
 void runQ3(double numIn) {
 	double num = numIn;
 	double runningNum = num;
 	double runningFact = 2;
-	int factNotFound = 0;
 
-	while (factNotFound == 0) {
+	while (0 == 0) {
 		if (remainder(runningNum, runningFact) == 0) {
 			runningNum /= runningFact;
 		}
