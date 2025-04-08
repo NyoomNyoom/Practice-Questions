80cb8ec6d91e69d79ff3f412d429d2447a5bb619
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-04-08 22:17:27 +1200

    Solving q2

--- ProjectEuler/ProjectEuler/Q2.C
+++ ProjectEuler/ProjectEuler/Q2.C
@@ -3,16 +3,20 @@
 
 // Find the sum of the even Fibanacci numbers under 4 million.
 int runQ2() {
-	int sum = 0;
+	int sum = 2; // Because I am starting at i=5.
 	int i1 = 3;
 	int i2 = 2;
 
-	for (int i = 5; i < 10; i = i1 + i2) {
-		printf("i1: %d, i2: %d, fib: %d", i1, i2, i);
+	for (int i = 5; i < 4000000; i = i1 + i2) {
+		if (i % 2 == 0) {
+			sum += i;
+		}
 
 		i2 = i1;
 		i1 = i;
 	}
+
+	printf("sum: %d", sum);
 
 	return 0;
 }
--- ProjectEuler/ProjectEuler/Q2.h
+++ ProjectEuler/ProjectEuler/Q2.h
@@ -2,9 +2,9 @@
 #define Q2_H  
 
 struct Q2 {
-	int someField;  
+	int someField;  // Just a placeholder to make Visual studio shut up
 };
 
 #endif
 
-int runQ2();
+int runQ2(); // The Defacto main method.
