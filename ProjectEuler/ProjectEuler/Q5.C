7de8450b5fae6b67d21316173a62f0b53dca5b63
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-04-11 01:14:21 +1200

    created the basic file needs :)

---
+++ ProjectEuler/ProjectEuler/Q5.C
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "Q5.h"
+
+void runQ5() {
+
+}
---
+++ ProjectEuler/ProjectEuler/Q5.h
@@ -0,0 +1,10 @@
+#ifndef Q5_H  
+#define Q5_H  
+
+struct Q5 {
+	int placeholder;  // Just a placeholder to make Visual studio shut up
+};
+
+#endif
+
+void runQ5(); // The Defacto main method.
